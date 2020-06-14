c57aaabdaa09f5ca6e302cc0d93cd00f24826f60
highonweb <mothish07122001@outlook.com>
2020-06-14 08:55:01 +0530

    Updates

---
+++ precache-manifest.a8d9a677b960b582ca51d52e11ef5b37.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10e0f970aa17dd50597e98fb33656742",
+    "url": "/Superhero/index.html"
+  },
+  {
+    "revision": "3c8e147ede10c6c21ff9",
+    "url": "/Superhero/static/css/main.629ece0e.chunk.css"
+  },
+  {
+    "revision": "0fcf664fb2a36a5d4f77",
+    "url": "/Superhero/static/js/2.9b7b890a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Superhero/static/js/2.9b7b890a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c8e147ede10c6c21ff9",
+    "url": "/Superhero/static/js/main.d3df4c8d.chunk.js"
+  },
+  {
+    "revision": "c657fa50f0eaf8877db1",
+    "url": "/Superhero/static/js/runtime-main.4ef7ba13.js"
+  },
+  {
+    "revision": "3970d2f84be49ab801b0c947235566be",
+    "url": "/Superhero/static/media/logo.3970d2f8.png"
+  }
+]);
